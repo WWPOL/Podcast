ff732b1ece063bfc9d07ef9d3936c2f6a15e8f26
Rudhra Raveendran <rudytheninja@gmail.com>
2020-12-10 20:36:38 -0500

    update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `The Barry Boys Show`,
+    description: `Homepage of The Barry Boys Show, the podcast of your favorite Barry Boys.`,
+    author: `@WWPOL`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
